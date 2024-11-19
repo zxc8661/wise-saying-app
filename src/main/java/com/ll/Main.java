1b0d6072eb4d5f7c0c54e52ed558c73afdf94c09
janjan <97529177+zxc8661@users.noreply.github.com>
2024-11-19 17:24:30 +0900

    feat explower, modify, add, list, delect

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -20,6 +20,12 @@ class WiseSaying{
         this.writer = writer;
         this.num = num;
     }
+    public void changeSaying(String saying){
+        this.saying=saying;
+    }
+    public void changeWriter(String writer){
+        this.writer=writer;
+    }
 }
 class App{
     List<WiseSaying> list = new ArrayList<>();
@@ -33,29 +39,61 @@ class App{
         while(flag){
             System.out.println("== 명언 앱 ==");
             System.out.printf("명령)");
-            String cmd = sc.nextLine();//가나다라
-            switch(cmd){
-                case "등록":
-                    add(count);
-                    break;
-                case "종료":
-                    flag=false;
-                    break;
-                case "목록":
-                    System.out.println("번호 / 작가 / 명언");
-                    for(int i=list.size()-1;i>=0;i--){
-                        WiseSaying tmp = list.get(i);
-                        System.out.println(tmp.num +" / " +tmp.writer+" / "+tmp.saying);
-                    }
-                    break;
-                case "삭제":
-
-                    break;
-
+            String total = sc.nextLine();//가나다라
+//            String cmd = total.substring(0,2);
+//            String rest = total.substring(2);
+            String []cmd = total.split("\\?");
+            try {
+                switch (cmd[0]) {
+                    case "등록":
+                        add(count);
+                        count++;
+                        break;
+                    case "종료":
+                        flag = false;
+                        break;
+                    case "목록":
+                        list();
+                        break;
+                    case "삭제":
+                        delect(cmd[1]);
+                        break;
+                    case "수정":
+                        modify(cmd[1]);
+                }
+            }catch (ArrayIndexOutOfBoundsException e){
+                System.out.println("잘못된 명령어를 입력하였습니다. 다시 입력해 주세요.");
             }
         }
     }
+    public int explower(int num){
+        for(int i=0;i<list.size();i++){
+            WiseSaying ws = list.get(i);
+            if(num==ws.num){
+                return i;
+            }
+        }
+        return -1;
+    }
+    public void modify(String token){
+        String[] tokens = token.split("=");
+        int num = Integer.parseInt(tokens[1]);
+        int index = explower(num);
+        if(index!=-1){
+            WiseSaying ws = list.get(index);
+            System.out.println("명언(기존) : "+ws.saying);
+            System.out.print("명언 : ");
+            String newsaying=sc.nextLine();
+            System.out.println("작가(기존) : "+ws.writer);
+            System.out.print("작가 : ");
+            String newwriter = sc.nextLine();
+            ws.changeSaying(newsaying);
+            ws.changeWriter(newwriter);
+        }else{
+            System.out.println(num+"번 명언은 존재하지 않습니다");
+        }
 
+    }
     public void add(int count){
         System.out.printf("명언 : ");
         String say = sc.nextLine();
@@ -64,5 +102,27 @@ class App{
         WiseSaying ws = new WiseSaying(say,writer,count++);
         list.add(ws);
         System.out.println(ws.num+"번 명언이 등록되었습니다.");
+    }
+    public void list(){
+        System.out.println("번호 / 작가 / 명언");
+        System.out.println("-------------------------");
+        for(int i=list.size()-1;i>=0;i--){
+            WiseSaying tmp = list.get(i);
+            System.out.println(tmp.num +" / " +tmp.writer+" / "+tmp.saying);
+        }
+    }
+    public void delect(String tmp){
+        String [] sarr = tmp.split("=");
+        int num =Integer.parseInt(sarr[1]);
+        int index = explower(num);
+        if(index!=-1){
+            list.remove(index);
+            System.out.println(num+"번 명언이 삭제되었습니다.");
+            return;
+        }else{
+            System.out.println(num+"번 명언은 존재하지 않습니다");
+        }
+
+
     }
 }
